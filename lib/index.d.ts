3bfd3b9344d2e1461f1dab1a46ebeae142a6b9e9
gkampitakis <gkabitakis@gmail.com>
2020-08-22 15:18:41 +0100

    docs: add comments on typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,8 +2,11 @@ import { FastifyPlugin } from 'fastify';
 import { addHealthCheck } from './src/customHealthCheck';
 
 export interface CustomHealthCheckOptions {
+  /** Path where health route is registered */
   path?: string;
+  /** Object where you can define custom information you would like to include in healthcheck response */
   info?: Record<string, string>;
+  /** Flag that enables additional information to be presented in health check object when a check fails. */
   exposeFailure?: boolean;
 }
 
--- lib/src/customHealthCheck.d.ts
+++ lib/src/customHealthCheck.d.ts
@@ -1,7 +1,13 @@
 export interface addHealthCheck {
+  /**  Decorator for adding custom health checks functions
+   * @param label string for registering checks, must be unique
+   * @param fn callback function supports Promises
+   * @param evaluation object containing a value to compare with healthcheck fn return value
+  */
   (label: string, fn: (...args: any) => Promise<unknown> | unknown, evaluation?: evaluation): void;
 }
 
 interface evaluation {
+  /** object or value to compare with healthcheck fn */
   value: unknown
 }
